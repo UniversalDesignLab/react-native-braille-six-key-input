74ad3e1de951547d074480591445aa8397aaffe6
Faddah Wolf <my_biz@me.com>
2018-10-12 17:04:00 -0700

    Removed RN Dimensions, COLORS & ifIPhone5s & width, no longer used.

--- Package/src/components/SixKeyInput.js
+++ Package/src/components/SixKeyInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Dimensions, View } from 'react-native'
+import { View } from 'react-native'
 import ResponsiveStylesheet from 'react-native-responsive-stylesheet'
 import PropTypes from 'prop-types'
 
@@ -7,10 +7,7 @@ import BrailleInput from './BrailleInput'
 import SpaceButton from './SpaceButton'
 import Done from './Done'
 
-import { COLORS, ifIPhone5s } from '../config'
 import { unicodeBrailleCharacters } from '../brailleObj'
-
-const { width } = Dimensions.get('screen')
 
 // Android Galaxy Tab-E Size: height: 961.5022957581195 width: 600.9389348488247
 
